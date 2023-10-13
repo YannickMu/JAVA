ae58b7df59ae6e90232919b12de276ada6ddf46e
YannickMüller <117479959+YannickMu@users.noreply.github.com>
2023-10-13 20:29:00 +0200

    sdf

--- src/Klassen.java
+++ src/Klassen.java
@@ -1,9 +1,15 @@
 public class Klassen {
     public static void main(String[] args) {
-        int x;
-        int y;
-        int z;
+        Vektor v = new Vektor();
+        Vektor v2 = new Vektor();
 
+        v.x = 1;
+        v.y = 0;
+        v.z = 0;
 
+        Vektor v3 = v.add(v2);
+
+        System.out.println(v3);
     }
 }
+
---
+++ src/KonstruktorenKlassen.java
@@ -0,0 +1,10 @@
+public class KonstruktorenKlassen {
+    public static void main(String[] args) {
+        Vektor v = new Vektor(1);
+        Vektor v2 = new Vektor(0,3);
+        v.z = 4;
+        Vektor v3 = v.add(v2);
+
+        System.out.println(v3);
+    }
+}
